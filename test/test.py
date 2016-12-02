ef430f980e3c2c2bb2a978843ddf408180421bb5
Xeclipse <249912350@qq.com>
2016-12-02 20:32:03 +0800

    add sorted importance

--- test/test.py
+++ test/test.py
@@ -7,9 +7,11 @@ print cou[1]
 '''
 
 file=open('../output/FSD_Importance')
-tmp=[]
-for i in file.readlines():
-    k=i.strip().split(':')
-    tmp.append([k[0],float(k[1])])
-k= sorted(tmp, cmp=lambda x,y: x[0]<y[0] , reverse=True)
-print k
+tmp=[(i.strip().split(':')) for i in file.readlines()]
+file.close()
+for i in tmp:
+    i[1]=float(i[1])
+k= sorted(tmp,cmp=lambda x,y : cmp(x[1],y[1]),reverse=True)
+file=open('../output/FSD_Importance_sorted','w')
+for i in k:
+    file.write(i[0]+':'+str(i[1])+'\n')
