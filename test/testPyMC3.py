9ef987cff13f396b40925000768c54d7bf92fbcd
Xeclipse <249912350@qq.com>
2017-03-18 23:41:34 +0800

    new start

---
+++ test/testPyMC3.py
@@ -0,0 +1,59 @@
+#coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Initialize random number generator
+np.random.seed(123)
+
+# True parameter values
+alpha, sigma = 1, 1
+beta = [1, 2.5]
+
+# Size of dataset
+size = 100
+
+# Predictor variable
+X1 = np.random.randn(size)
+X2 = np.random.randn(size) * 0.2
+
+# Simulate outcome variable
+Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+
+
+
+fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,4))
+axes[0].scatter(X1, Y)
+axes[1].scatter(X2, Y)
+axes[0].set_ylabel('Y'); axes[0].set_xlabel('X1'); axes[1].set_xlabel('X2');
+plt.show()
+
+
+from pymc3 import Model, Normal, HalfNormal
+basic_model = Model()
+with basic_model:
+
+    # Priors for unknown model parameters
+    alpha = Normal('alpha', mu=0, sd=10)
+    beta = Normal('beta', mu=0, sd=10, shape=2)
+    sigma = HalfNormal('sigma', sd=1)
+
+    # Expected value of outcome
+    mu = alpha + beta[0]*X1 + beta[1]*X2
+
+    # Likelihood (sampling distribution) of observations
+    Y_obs = Normal('Y_obs', mu=mu, sd=sigma, observed=Y)
+
+'''
+from pymc3 import find_MAP
+
+map_estimate = find_MAP(model=basic_model)
+
+print(map_estimate)
+'''
+
+from pymc3 import NUTS, sample
+from scipy import optimize
+
+with basic_model:
+    # draw 2000 posterior samples
+    trace = sample(2000)
